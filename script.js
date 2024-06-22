73ec737753abd2d7b8f7fdf6cd47f03f8556c0f8
Daniel Wilson <danwilson501@gmail.com>
2024-06-21 20:15:37 -0400

    dimensions added to SVG module with data set within the javascript file.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // U5223-1368
+
+const data = [100, 420, 230, 850, 560, 925];
+
+const svgWidth = 500;
+const height = 20;
+const margin = 1;
+const svgHeight = data.length * (height + margin);
+
+const svg = d3.select("#chart")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
